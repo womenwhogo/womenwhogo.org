a9da36529700f2215ea17018ec6d3b7fe1f839f9
Maartje Eyskens <meyskens@me.com>
2017-08-01 19:04:07 +0200

    http FileServer knows about index.html

--- storage.go
+++ storage.go
@@ -7,10 +7,6 @@ import (
 func handleStatic(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Served-With-Love-By", "Gophers")
 
-	if r.URL.Path == "/" {
-		r.URL.Path = "/index.html"
-	}
-
 	http.FileServer(http.Dir("./womenwhogo.org/")).ServeHTTP(w, r)
 }
 
